8664b13a065f6a866d9e7dc910c538a30883cc0c
project2003 <projectalgoritmika123@gmail.com>
2022-11-14 19:07:05 +0400

    addtask

---
+++ MultipleFile-Upload/front-to-back/Controllers/AboutController.cs
@@ -0,0 +1,25 @@
+﻿using front_to_back.DAL;
+using front_to_back.ViewModels.About;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace front_to_back.Controllers
+{
+    [Authorize]
+    public class AboutController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public AboutController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MultipleFile-Upload/front-to-back/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace front_to_back.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            CategoryComponents = new List<CategoryComponent>();
+        }
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Ad mutleq doldurumalidir"),MinLength(3,ErrorMessage ="Adin uzunluqu minimum 3 olmalidir")]
+        public string Title { get; set; }
+        public CategoryStatus Status { get; set; }
+        public ICollection<CategoryComponent> CategoryComponents { get; set; }
+    }
+
+    public enum CategoryStatus
+    {
+        Sale,
+        New,
+
+    }
+}
